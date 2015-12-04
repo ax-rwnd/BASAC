13be394d4aa678fd5b53de7cc905c79a09a0b5a3
weedz <weedz@localhost>
2015-12-04 11:40:00 +0100

    Bluetooth

--- android/app/src/main/java/d0020e/basac/HomeScreenActivity.java
+++ android/app/src/main/java/d0020e/basac/HomeScreenActivity.java
@@ -1,19 +1,28 @@
 package d0020e.basac;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeScreenActivity extends AppCompatActivity {
+    public static int BLUETOOTH_REQUEST_CODE = 1;
+    private BroadcastReceiver mBluetoothReceiver;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
@@ -28,6 +37,36 @@ public class HomeScreenActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
+                    final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    updateBluetoothStatus(state);
+                }
+            }
+        };
+        this.registerReceiver(mBluetoothReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+        updateBluetoothStatus(mBluetoothAdapter.getState());
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        this.unregisterReceiver(mBluetoothReceiver);
+    }
+
+    private void updateBluetoothStatus(int state) {
+        switch (state) {
+            case BluetoothAdapter.STATE_OFF:
+                Toast.makeText(getApplicationContext(), "Bluetooth off", Toast.LENGTH_SHORT).show();
+                break;
+            case BluetoothAdapter.STATE_ON:
+                Toast.makeText(getApplicationContext(), "Bluetooth on", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     @Override
--- android/app/src/main/java/d0020e/basac/SettingsScreenActivity.java
+++ android/app/src/main/java/d0020e/basac/SettingsScreenActivity.java
@@ -1,11 +1,15 @@
 package d0020e.basac;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class SettingsScreenActivity extends AppCompatActivity {
 
@@ -24,6 +28,55 @@ public class SettingsScreenActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was checked
+        switch(view.getId()) {
+            case R.id.checkbox_1:
+                if (checked) {
+
+                }
+                else {
+
+                }
+                break;
+            case R.id.checkbox_2:
+                if (checked) {
+
+                }
+                else {
+
+                }
+                break;
+        }
+    }
+
+    public void toggle_bluetooth(View view) {
+        BluetoothAdapter mBlueToothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBlueToothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Bluetooth not available", Toast.LENGTH_SHORT).show();
+        } else {
+            if (!mBlueToothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, HomeScreenActivity.BLUETOOTH_REQUEST_CODE);
+            } else {
+                mBlueToothAdapter.disable();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == HomeScreenActivity.BLUETOOTH_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth not enabled", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
