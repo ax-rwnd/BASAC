8050b3dd4a91eae12a16fc224bcd7d7ae676d746
Sebastian Emmot <sebastian.emmot@gmail.com>
2015-12-09 15:45:32 +0100

    Added DataModel and StateController
    added structure for the data model and controller.

--- android/app/src/main/java/d0020e/basac/DataScreenActivity.java
+++ android/app/src/main/java/d0020e/basac/DataScreenActivity.java
@@ -6,27 +6,58 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
-public class DataScreenActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
+
+public class DataScreenActivity extends AppCompatActivity implements Observer {
     private ProgressBar oxygenProgress;
+    private DataModel dataModel;
+    private Button dataButton;
+
+    public static int isRunning = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_screen);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+
+        Bundle data = getIntent().getExtras();
+        dataModel = (DataModel) data.getSerializable("dataModel");
+        dataModel.addObserver(this);
 
         oxygenProgress = (ProgressBar) findViewById(R.id.oxygen_bar);
+        oxygenProgress.setProgress(dataModel.getTestValue());
 
+        dataButton = (Button) findViewById(R.id.action_progress);
+
+        this.isRunning = 1;
     }
 
 
-    public void updateProgressbar(View view) {
-        oxygenProgress.incrementProgressBy(1);
-       // oxygenProgress.setProgress(50);
+    public void updateProgressbar() {
+        oxygenProgress.setProgress(dataModel.getTestValue());
     }
 
+    public void incrementProgressbar(View view) {
+        dataModel.incrementTestValue();
+    }
+
+    public void update(Observable observable, Object data) {
+            this.updateProgressbar();
+            Log.e("update", "fubar");
+    }
+    public void onStop() {
+        super.onStop();
+        this.isRunning = 2;
+    }
+
+    public void onStart() {
+        super.onStart();
+        this.isRunning = 1;
+    }
 }
