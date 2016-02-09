0107fefecca8c2c217be54064cb005892da1b7f5
Linus Björklund <weedzcokie@gmail.com>
2016-02-09 02:11:08 +0100

    Show values in data screen

--- android/app/src/main/java/d0020e/basac/DataScreenActivity.java
+++ android/app/src/main/java/d0020e/basac/DataScreenActivity.java
@@ -49,27 +49,39 @@ public class DataScreenActivity extends AppCompatActivity implements Observer {
 
     public void updateOxygenbar() {
         ProgressBar bar = (ProgressBar) findViewById(R.id.oxygen_bar);
-        bar.setProgress((int) Math.round(DataModel.getInstance().getValue(DataStore.VALUE_OXYGEN)));
+        bar.setProgress((int)DataModel.getInstance().getValue(DataStore.VALUE_OXYGEN));
+        TextView text = (TextView) findViewById(R.id.oxygen_value);
+        text.setText("Oxygen: " + DataModel.getInstance().getValue(DataStore.VALUE_OXYGEN) + "%");
     }
     public void updateTemperatureBar() {
         ProgressBar bar = (ProgressBar) findViewById(R.id.temperature_bar);
-        bar.setProgress((int) Math.round(DataModel.getInstance().getValue(DataStore.VALUE_TEMPERATURE)));
+        bar.setProgress((int)DataModel.getInstance().getValue(DataStore.VALUE_TEMPERATURE));
+        TextView text = (TextView) findViewById(R.id.temperature_value);
+        text.setText("Temperature: " + DataModel.getInstance().getValue(DataStore.VALUE_TEMPERATURE) + " C");
     }
     public void updateHeartRateBar() {
         ProgressBar bar = (ProgressBar) findViewById(R.id.heartrate_bar);
-        bar.setProgress((int) Math.round(DataModel.getInstance().getValue(DataStore.VALUE_HEARTRATE)));
+        bar.setProgress((int)DataModel.getInstance().getValue(DataStore.VALUE_HEARTRATE));
+        TextView text = (TextView) findViewById(R.id.heartrate_value);
+        text.setText("Heart rate: " + DataModel.getInstance().getValue(DataStore.VALUE_HEARTRATE) + " bpm");
     }
     public void updateAirPressureBar() {
         ProgressBar bar = (ProgressBar) findViewById(R.id.airpressure_bar);
-        bar.setProgress((int) Math.round(DataModel.getInstance().getValue(DataStore.VALUE_AIRPRESSURE)));
+        bar.setProgress((int)DataModel.getInstance().getValue(DataStore.VALUE_AIRPRESSURE));
+        TextView text = (TextView) findViewById(R.id.airpressure_value);
+        text.setText("Air pressure: " + DataModel.getInstance().getValue(DataStore.VALUE_AIRPRESSURE) + " Pa");
     }
     public void updateHumidityBar() {
         ProgressBar bar = (ProgressBar) findViewById(R.id.humidity_bar);
-        bar.setProgress((int) Math.round(DataModel.getInstance().getValue(DataStore.VALUE_HUMIDITY)));
+        bar.setProgress((int)DataModel.getInstance().getValue(DataStore.VALUE_HUMIDITY));
+        TextView text = (TextView) findViewById(R.id.humidity_value);
+        text.setText("Humidity: " + DataModel.getInstance().getValue(DataStore.VALUE_HUMIDITY) + "%");
     }
     public void updateCoBar() {
         ProgressBar bar = (ProgressBar) findViewById(R.id.co_bar);
-        bar.setProgress((int) Math.round(DataModel.getInstance().getValue(DataStore.VALUE_CO)));
+        bar.setProgress((int)DataModel.getInstance().getValue(DataStore.VALUE_CO));
+        TextView text = (TextView) findViewById(R.id.co_value);
+        text.setText("CO: " + DataModel.getInstance().getValue(DataStore.VALUE_CO) + "%");
     }
 
     public void update(Observable observable, Object data) {
