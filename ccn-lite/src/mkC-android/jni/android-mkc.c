71fedd61b753c1b5b231af8690863261a54b5c00
ax-rwnd <axel-sundbom@hotmail.com>
2016-03-02 10:29:39 +0000

    Adds prebuilts stable android-mkc.

--- ccn-lite/src/mkC-android/jni/android-mkc.c
+++ ccn-lite/src/mkC-android/jni/android-mkc.c
@@ -1,7 +1,3 @@
-/* The BASAC port of ccn-lite-mkC for android.
- * TODO: License
- */
-
 /*
  * @f util/ccn-lite-mkC.c
  * @b CLI mkContent, write to Stdout
@@ -22,6 +18,7 @@
  *
  * File history:
  * 2013-07-06  created
+ * 2016-02-19  android port uploaded to github -- ax-rwnd
  */
 
 #define USE_SUITE_CCNB
@@ -45,117 +42,143 @@
 char *private_key_path;
 char *witness;
 
-JNIEXPORT jint JNICALL Java_com_basac_androidmkc_AndroidmkC_generateContent(JNIEnv *env, jobject jthis, jstring content, jstring ipath, jstring opath) {
-    const char *ncontent = (*env)->GetStringUTFChars(env, content, 0);
-    const char *npath = (*env)->GetStringUTFChars(env, opath, 0);
-    const char *nipath = (*env)->GetStringUTFChars(env, ipath, 0);
+JNIEXPORT jint JNICALL Java_d0020e_basac_StateController_generateContent(JNIEnv *env, jobject jthis, jstring prefix, jstring ipath, jstring opath, jint ccn_suite) {
+	//extracts c strings from jstrings passed in arglist
+	const char *nprefix = (*env)->GetStringUTFChars(env, prefix, 0);
+	const char *npath = (*env)->GetStringUTFChars(env, opath, 0);
+	const char *nipath = (*env)->GetStringUTFChars(env, ipath, 0);
 
-    char *hwstr = malloc(strlen(ncontent));
-    memcpy(hwstr, ncontent, strlen(ncontent));
+	//input/output filenames
+	const char *infname = nipath;
+	const char *outfname = npath;
 
+	unsigned char body[64*1024];
+	unsigned char out[65*1024];
+	unsigned char *publisher = out;
+	unsigned int chunknum = UINT_MAX, lastchunknum = UINT_MAX;
 
-    unsigned char body[64*1024];
-    unsigned char out[65*1024];
-    unsigned char *publisher = out;
-    const char *infname = nipath;//0;
-    const char *outfname = npath;
-    unsigned int chunknum = UINT_MAX, lastchunknum = UINT_MAX;
-    int f, len, opt, plen, offs = 0;
-    struct ccnl_prefix_s *name;
-    int suite = CCNL_SUITE_DEFAULT;
-    struct key_s *keys = NULL;
+	//file vars
+	int f, len, opt, plen, offs = 0;
+
+	//content prefix
+	struct ccnl_prefix_s *name;
+
+	//set suite here, CCNL_SUITE_DEFAULT, CCNL_SUITE_NDNTLV
+	int suite = (ccn_suite<0)?CCNL_SUITE_DEFAULT:ccn_suite;
+	struct key_s *keys = NULL;
 
 	//reads an input file to *out
-    if (infname) {
-        f = open(infname, O_RDONLY);
-        if (f < 0)
-            perror("file open:");
-    } else
-        f = 0;
-    len = read(f, body, sizeof(body));
-    close(f);
-    memset(out, 0, sizeof(out));
-    //char *argv[1] = {"hello world"};
-    name = ccnl_URItoPrefix(hwstr, suite, NULL, chunknum == UINT_MAX ? NULL : &chunknum);
-    //name = ccnl_URItoPrefix(argv[optind], suite, argv[optind+1],
-    //                           chunknum == UINT_MAX ? NULL : &chunknum);
+	if (infname) {
+		f = open(infname, O_RDONLY);
+		if (f < 0) {
+			perror("file open:");
+		}
+	} else {
+		perror("no terminal support on android.");
+		return -1;
+	}
 
-    switch (suite) {
-    
+	len = read(f, body, sizeof(body));
+	close(f);
+	memset(out, 0, sizeof(out));
+
+
+	//create mutable string for URItoPrefix...
+	char *mutstr = malloc(strlen(nprefix));
+	memcpy(mutstr, nprefix, strlen(nprefix));
+	name = ccnl_URItoPrefix(mutstr, suite, NULL, chunknum == UINT_MAX ? NULL : &chunknum);
+	free(mutstr);
+
+	//fill with content
+	switch (suite) {
+	
 #ifdef USE_SUITE_CCNB
-    case CCNL_SUITE_CCNB:
-        len = ccnl_ccnb_fillContent(name, body, len, NULL, out);
-        break;
+	case CCNL_SUITE_CCNB:
+		len = ccnl_ccnb_fillContent(name, body, len, NULL, out);
+		break;
 #endif
 #ifdef USE_SUITE_CCNTLV
-    case CCNL_SUITE_CCNTLV:
+	case CCNL_SUITE_CCNTLV:
 
-        offs = CCNL_MAX_PACKET_SIZE;
-        if (keys) {
-            unsigned char keyval[64];
-            unsigned char keyid[32];
-            // use the first key found in the key file
-            ccnl_hmac256_keyval(keys->key, keys->keylen, keyval);
-            ccnl_hmac256_keyid(keys->key, keys->keylen, keyid);
-            len = ccnl_ccntlv_prependSignedContentWithHdr(name, body, len,
-                  lastchunknum == UINT_MAX ? NULL : &lastchunknum,
-                  NULL, keyval, keyid, &offs, out);
-        } else
-            len = ccnl_ccntlv_prependContentWithHdr(name, body, len,
-                          lastchunknum == UINT_MAX ? NULL : &lastchunknum,
-                          NULL  /*Int *contentpos */, &offs, out);
-        break;
+		offs = CCNL_MAX_PACKET_SIZE;
+		if (keys) {
+			unsigned char keyval[64];
+			unsigned char keyid[32];
+
+			// use the first key found in the key file
+			ccnl_hmac256_keyval(keys->key, keys->keylen, keyval);
+			ccnl_hmac256_keyid(keys->key, keys->keylen, keyid);
+
+			len = ccnl_ccntlv_prependSignedContentWithHdr(name, body, len,
+				  lastchunknum == UINT_MAX ? NULL : &lastchunknum,
+				  NULL, keyval, keyid, &offs, out);
+		} else
+			len = ccnl_ccntlv_prependContentWithHdr(name, body, len,
+						  lastchunknum == UINT_MAX ? NULL : &lastchunknum,
+						  NULL  /*Int *contentpos */, &offs, out);
+		break;
 #endif
 #ifdef USE_SUITE_CISTLV
-    case CCNL_SUITE_CISTLV:
-        offs = CCNL_MAX_PACKET_SIZE;
-        len = ccnl_cistlv_prependContentWithHdr(name, body, len,
-                  lastchunknum == UINT_MAX ? NULL : &lastchunknum,
-                  NULL, &offs, out);
-        break;
+	case CCNL_SUITE_CISTLV:
+		offs = CCNL_MAX_PACKET_SIZE;
+		len = ccnl_cistlv_prependContentWithHdr(name, body, len,
+			 lastchunknum == UINT_MAX ? NULL : &lastchunknum,
+			 NULL, &offs, out);
+		break;
 #endif
 #ifdef USE_SUITE_IOTTLV
-    case CCNL_SUITE_IOTTLV:
-        offs = CCNL_MAX_PACKET_SIZE;
-        if (ccnl_iottlv_prependReply(name, body, len, &offs, NULL,
-                   lastchunknum == UINT_MAX ? NULL : &lastchunknum, out) < 0
-              || ccnl_switch_prependCoding(CCNL_ENC_IOT2014, &offs, out) < 0)
-            return -1;
-        len = CCNL_MAX_PACKET_SIZE - offs;
-        break;
+	case CCNL_SUITE_IOTTLV:
+		offs = CCNL_MAX_PACKET_SIZE;
+		if (ccnl_iottlv_prependReply(name, body, len, &offs, NULL,
+			lastchunknum == UINT_MAX ? NULL : &lastchunknum, out) < 0
+			|| ccnl_switch_prependCoding(CCNL_ENC_IOT2014, &offs, out) < 0) {
+			return -1;
+		}
+
+		len = CCNL_MAX_PACKET_SIZE - offs;
+		break;
 #endif
 
 #ifdef USE_SUITE_NDNTLV
-    case CCNL_SUITE_NDNTLV:
-        offs = CCNL_MAX_PACKET_SIZE;
-        if (keys) {
-            unsigned char keyval[64];
-            unsigned char keyid[32];
-            // use the first key found in the key file
-            ccnl_hmac256_keyval(keys->key, keys->keylen, keyval);
-            ccnl_hmac256_keyid(keys->key, keys->keylen, keyid);
-            len = ccnl_ndntlv_prependSignedContent(name, body, len,
-                  lastchunknum == UINT_MAX ? NULL : &lastchunknum,
-                  NULL, keyval, keyid, &offs, out);
-        } else {
-            len = ccnl_ndntlv_prependContent(name, body, len,
-                  NULL, lastchunknum == UINT_MAX ? NULL : &lastchunknum,
-                  &offs, out);
-        }
-        break;
-#endif
-    default:
-        break;
-    }
+	case CCNL_SUITE_NDNTLV:
+		offs = CCNL_MAX_PACKET_SIZE;
+		if (keys) {
+			unsigned char keyval[64];
+			unsigned char keyid[32];
 
-    if (outfname) {
-        f = creat(outfname, 0666);
-        if (f < 0)
-            perror("file open:");
-    } else
-        f = 1;
-    write(f, out + offs, len);
-    close(f);
-    return 0;
+			// use the first key found in the key file
+			ccnl_hmac256_keyval(keys->key, keys->keylen, keyval);
+			ccnl_hmac256_keyid(keys->key, keys->keylen, keyid);
+
+			len = ccnl_ndntlv_prependSignedContent(name, body, len,
+				  lastchunknum == UINT_MAX ? NULL : &lastchunknum,
+				  NULL, keyval, keyid, &offs, out);
+		} else {
+			len = ccnl_ndntlv_prependContent(name, body, len,
+				  NULL, lastchunknum == UINT_MAX ? NULL : &lastchunknum,
+				  &offs, out);
+		}
+
+		break;
+#endif
+	default:
+		break;
+	}
+
+	//write to output file
+	if (outfname) {
+		f = creat(outfname, 0666);
+		if (f < 0) {
+			perror("file open:");
+		}
+	} else {
+		//file = stdout
+		f = 1;
+	}
+
+	write(f, out + offs, len);
+	close(f);
+
+	return 0;
 }
 
